f83dbfccba393350434a2ccc5dc7e3fa31c01981
XenialDan <thebigsmilexd@gmail.com>
2017-06-10 13:50:08 +0200

    Create Travis.sh

--- Travis.sh
+++ Travis.sh
@@ -13,9 +13,10 @@ cp -r tests/plugins plugins
 "$PHP_BINARY" ./plugins/PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./plugins/PocketMine-DevTools --relative ./plugins/PocketMine-DevTools --out ./plugins/DevTools.phar
 rm -rf ./plugins/PocketMine-DevTools
 
-echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
+echo -e "version\nplugins\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
 if ls plugins/DevTools/*PocketMine*.phar >/dev/null 2>&1; then
     echo Server phar created successfully.
+    exit 0
 else
     echo No phar created!
     exit 1
